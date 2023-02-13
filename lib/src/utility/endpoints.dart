b988d06242ac3a692c99aaaf62e5d5cc802be24f
Souksakhone Sengsaya <souksakhone.litd@gmail.com>
2023-02-13 14:16:15 +0700

    🐞 fix(change base version): change version
    migrate to v2

--- lib/src/utility/endpoints.dart
+++ lib/src/utility/endpoints.dart
@@ -1,6 +1,9 @@
 part of tenor;
 
-const _tenorBaseUrl = 'https://g.tenor.com/v1/';
+// old api
+// const _tenorBaseUrl = 'https://g.tenor.com/v1/';
+
+const _tenorBaseUrl = 'https://tenor.googleapis.com/v2/';
 
 enum EndPoint {
   anonid,
